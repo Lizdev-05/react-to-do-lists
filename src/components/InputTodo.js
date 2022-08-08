ed358164df215505ca686ae70fca32852a71065e
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-08 20:39:46 +0100

    complete-set up

---
+++ src/components/InputTodo.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+
+const InputTodo = props => {
+  const [title, setTitle] = useState("")
+
+  const onChange = e => {
+    setTitle(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (title.trim()) {
+      props.addTodoProps(title)
+      setTitle("")
+    } else {
+      alert("Please write item")
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
+  )
+}
+
+export default InputTodo
