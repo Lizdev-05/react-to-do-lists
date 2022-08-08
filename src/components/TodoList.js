7506ac426a6e67b6bd0c309cf0585e43f89c3df4
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-08 21:09:07 +0100

    refactor todoList to use functional component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,19 @@
-import React, { Component } from "react";
-import TodoItem from "./TodoItem";
+import React from "react"
+import TodoItem from "./TodoItem"
 
-class TodoList extends Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map((todo) => (
-         <TodoItem
-         key={todo.id}
-         todo={todo}
-         handleChangeProps={this.props.handleChangeProps}
-         deleteTodoProps={this.props.deleteTodoProps}
-         setUpdate={this.props.setUpdate}
-       />
-        ))}
-      </ul>
-    );
-  }
+const TodosList = props => {
+  return (
+    <ul>
+      {props.todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+          setUpdate={props.setUpdate}
+        />
+      ))}
+    </ul>
+  )
 }
-
-export default TodoList;
+export default TodosList
