95c41f22eb2d821243736b1261ce56e8adb5d091
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-08 20:41:17 +0100

    refactor-input-todo

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,34 +1,48 @@
 import React, { useState } from "react"
 
 const InputTodo = props => {
-  const [title, setTitle] = useState("")
+  const [inputText, setInputText] = useState({
+    fName: "",
+    lastName: "",
+  })
 
   const onChange = e => {
-    setTitle(e.target.value)
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    })
   }
 
   const handleSubmit = e => {
     e.preventDefault()
-    if (title.trim()) {
-      props.addTodoProps(title)
-      setTitle("")
-    } else {
-      alert("Please write item")
-    }
+    console.log("submitted")
   }
 
   return (
-    <form onSubmit={handleSubmit} className="form-container">
-      <input
-        type="text"
-        className="input-text"
-        placeholder="Add todo..."
-        value={title}
-        name="title"
-        onChange={onChange}
-      />
-      <button className="input-submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add first name"
+          value={inputText.fName}
+          name="fName"
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add last name"
+          value={inputText.lastName}
+          name="lastName"
+          onChange={onChange}
+        />
+
+        <button className="input-submit">Submit</button>
+      </form>
+      <h2>{inputText.fName}</h2>
+      <h2>{inputText.lastName}</h2>
+    </>
   )
 }
 
