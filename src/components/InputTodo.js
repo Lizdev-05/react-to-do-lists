d094bd09271f4f5428f6b79bb88a27f7d6c97ca9
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-08 20:44:40 +0100

    manage state with functional component

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -2,48 +2,40 @@ import React, { useState } from "react"
 
 const InputTodo = props => {
   const [inputText, setInputText] = useState({
-    fName: "",
-    lastName: "",
+    title: "",
   })
 
   const onChange = e => {
-    setInputText(prevState => {
-      return {
-        ...prevState,
-        [e.target.name]: e.target.value,
-      }
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
     })
   }
+
   const handleSubmit = e => {
     e.preventDefault()
-    console.log("submitted")
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title)
+      setInputText({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add first name"
-          value={inputText.fName}
-          name="fName"
-          onChange={onChange}
-        />
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add last name"
-          value={inputText.lastName}
-          name="lastName"
-          onChange={onChange}
-        />
-
-        <button className="input-submit">Submit</button>
-      </form>
-      <h2>{inputText.fName}</h2>
-      <h2>{inputText.lastName}</h2>
-    </>
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
   )
 }
 
