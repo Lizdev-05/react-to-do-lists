ca2dd9e24fdd8fdcb48e3b8c12cb61c034abcb2a
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-08 21:31:56 +0100

    replace delete input with icon

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import styles from "./TodoItem.module.css"
+import { FaTrash } from "react-icons/fa"
 
 const TodoItem = props => {
   const [editing, setEditing] = useState(false)
@@ -46,7 +47,7 @@ const TodoItem = props => {
           checked={completed}
           onChange={() => props.handleChangeProps(id)}
         />
-        <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+       <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
         <span style={completed ? completedStyle : null}>{title}</span>
       </div>
       <input
