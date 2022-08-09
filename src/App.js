b3be739e6adfdb125ba2706286887363a0500484
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-09 12:12:23 +0100

    use nesting

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './pages/About'
 import NotMatch from './pages/NotMatch'
 import TodoContainer from './components/TodoContainer'
 import Navbar from './components/Navbar'
-// import SinglePage from './pages/SinglePage'
+import SinglePage from './pages/SinglePage'
 
 const App = () => {
 
@@ -15,7 +15,7 @@ const App = () => {
 			<Routes>
 				<Route exact path='/' element={<TodoContainer />} />
 				<Route path='about' element={<About />}>
-					{/* <Route path=':slug' element={<SinglePage />} /> */}
+					<Route path=':slug' element={<SinglePage />} />
 				</Route>
 				<Route path='*' element={<NotMatch />} />
 			</Routes>
