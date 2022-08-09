e4524863659132e0ba01af8c0bdf40d4c1a4b7c7
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-09 10:56:31 +0100

    Implemment Link usage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,32 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const links = [
-		{
-			id: 1,
-			path: '/',
-			text: 'Home',
-		},
-		{
-			id: 2,
-			path: '/about',
-			text: 'About',
-		},
-	]
+    {
+      id: 1,
+      path: "/",
+      text: "Home",
+    },
+    {
+      id: 2,
+      path: "/about",
+      text: "About",
+    },
+  ];
 
-  return <div>Hello from Navbar</div>
-}
-export default Navbar
+  return (
+    <nav className="navBar">
+    <ul>
+      {links.map(link => {
+        return (
+          <li key={link.id}>
+            <Link to={link.path}>{link.text}</Link>
+          </li>
+        )
+      })}
+    </ul>
+  </nav>
+  );
+};
+export default Navbar;
